59802ee21fb8b943bdc8e2ba85b1957c7da9a74b
qwerkilll <alban.munoz40@gmail.com>
2023-03-27 15:34:23 +0200

    hello

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 mongoose.set('strictQuery', true);
 module.exports = async () => {
     try {
-        await mongoose.connect(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_ADDRESSE}:${process.env.MONGO_PORT}`)
+        await mongoose.connect(`mongodbsrv://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_ADDRESSE}`)
         console.log('Connected to MongoDB')
     } catch (error) {
         console.log('Error connecting to MongoDB', error.message)
